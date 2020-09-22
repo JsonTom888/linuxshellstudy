7eed05d3bf665a779e9447f5576c74b74be52d7e
JsonTom888 <Good666888@163.com>
2020-09-22 22:24:42 +0800

    while循环练习

---
+++ while.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# while loops
+
+n=1
+while [ $n -le 10 ]
+do
+    echo "$n"
+    # n=$(( n+1 )) # n增加1
+    # (( n++ ))    # n增加1
+    ((++n))        # n增加1
+done
+
+s=1
+total=0
+while (( $s <= 100 ))
+do
+    total=$((total + s ))
+    (( s++ ))
+done
+echo "1加到100的值 $total"
